aa06bbfe624332d2c6fe83f1fbfbe1be59a121b3
jb caron <jbcaron.dev@gmail.com>
2023-01-15 19:09:30 +0100

    Never gonna tell a lie and hurt you

--- include/msg_cli.h
+++ include/msg_cli.h
@@ -6,14 +6,12 @@
 /*   By: jcaron <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 18:19:05 by jcaron            #+#    #+#             */
-/*   Updated: 2023/01/05 18:47:05 by jcaron           ###   ########.fr       */
+/*   Updated: 2023/01/15 18:51:37 by jcaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MSG_CLI_H
 # define MSG_CLI_H
-
-extern int		g_ack;
 
 void	send_byte(int pid, char byte);
 void	send_msg(int pid, char *msg);
--- include/msg_srv.h
+++ include/msg_srv.h
@@ -6,18 +6,14 @@
 /*   By: jcaron <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 18:11:06 by jcaron            #+#    #+#             */
-/*   Updated: 2023/01/11 16:25:27 by jcaron           ###   ########.fr       */
+/*   Updated: 2023/01/15 19:03:44 by jcaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MSG_SRV_H
 # define MSG_SRV_H
 
-#include "libft.h"
-
 # define ESC 27
-
-extern int	g_rec_bit;
 
 char	get_byte(void);
 void	get_msg(void);
